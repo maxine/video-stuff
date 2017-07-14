34cdc9374f3382a611c7f5d275fa4fd15da076a5
Maxine Whitely <m.whitely16@gmail.com>
2017-07-14 11:14:36 -0500

    changes to writeclips function

--- writeclips.py
+++ writeclips.py
@@ -4,35 +4,50 @@ from configparser import SafeConfigParser
 parser = SafeConfigParser()
 test = parser.read('video-data.ini')
 
-concatClip = [i for i in range(len(parser.sections()))]
+#concatClip = [i for i in range(len(parser.sections()))]
 
+holdVideo = None
+finalVideo = None
 def writeClips():
 	for m in parser.sections():
 		text = parser.get(m, 'clip_text')
 		media = parser.get(m, 'media_link')
-		fSize = parser.get(m, 'font_size')
+		fSize = parser.getint(m, 'font_size')
 		fColor = parser.get(m, 'font_color')
 		textSpot = parser.get(m, 'text_location')
-		clipStart = parser.get(m, 'start_time')
-		clipEnd = parser.get(m, 'end_time')
+		clipStart = parser.getint(m, 'start_time')
+		clipEnd = parser.getint(m, 'end_time')
 		duration = clipStart - clipEnd
+
+		global holdVideo
+		global finalVideo
 
 		clip = VideoFileClip(media).subclip(clipStart, clipEnd)
 		txt_clip = TextClip(text,fontsize=fSize,color=fColor)
 		txt_clip = txt_clip.set_pos(textSpot).set_duration(10)
 		videoClip = CompositeVideoClip([clip, txt_clip])
-		concatClip[i] = videoClip
 
-print(concatClip)
+		if holdVideo == None:
+			holdVideo = videoClip
+			finalVideo = videoClip
+			videoClip = None
+			#holdVideo.write_videofile("yeahtest.mp4")
+		else:
+			holdVideo = finalVideo
+			finalVideo = concatenate_videoclips(holdVideo, videoClip)
+			finalVideo.write_videofile("yeahtest1.mp4")
+			videoClip = None
+		
+	#holdVideo.write_videofile("yeahtest.mp4")
+		
+writeClips()
+
+
+
+#holdVideo.write_videofile("yeahtest.mp4")
+
+#print(concatClip)
 #video = concatenate_videoclips(concatClip)
-#video.write_videofile("yeahtest.mp4")
 
-#NEXT TIME: GET VIDEO TO RENDER
-#error won't render video because "Traceback (most recent call last):
- # File "writeclips.py", line 27, in <module>
-  #  video = concatenate_videoclips(concatClip)
- # File "/Users/mwhitely/.virtualenvs/ffmpeg2/lib/python3.6/site-packages/moviepy/video/compositing/concatenate.py", line 71, in concatenate_videoclips
-  #  tt = np.cumsum([0] + [c.duration for c in clips])
- # File "/Users/mwhitely/.virtualenvs/ffmpeg2/lib/python3.6/site-packages/moviepy/video/compositing/concatenate.py", line 71, in <listcomp>
-  #  tt = np.cumsum([0] + [c.duration for c in clips])
-#AttributeError: 'int' object has no attribute 'duration'"
+#took out duration in txt_clip = txt_clip.set_pos(textSpot)
+#used to be: txt_clip = txt_clip.set_pos(textSpot).duration(duration)
